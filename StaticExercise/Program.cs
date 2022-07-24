cab7746e2a0965253ab3c7ebb502e7982d9c6d48
Jayden Callan <jqcallan04@gmail.com>
2022-07-24 17:35:30 -0600

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"10 degrees C to F: {TempConverter.CelsiusToFahrenheit(10)}");
+            Console.WriteLine($"10 degrees F to C: {TempConverter.FahrenheitToCelsius(10)}");
+        }
+    }
+}
